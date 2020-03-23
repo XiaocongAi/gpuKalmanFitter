af7a4eec391eceff0b74f68fa219dbc96292ef28
xai <xiaocong.ai@cern.ch>
2020-03-23 00:05:49 -0700

    Add option for writing

--- main.cu
+++ main.cu
@@ -4,6 +4,7 @@
 #include <thrust/device_vector.h>
 
 #include <chrono>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <random>
@@ -11,14 +12,14 @@
 #include <vector>
 
 static void show_usage(std::string name) {
-  std::cerr
-      << "Usage: <option(s)> VALUES"
-      << "Options:\n"
-      << "\t-h,--help\t\tShow this help message\n"
-      << "\t-t,--tracks \tSpecify the number of tracks\n"
-      << "\t-o,--output \tSpecify the output path for propagation results\n"
-      << "\t-d,--device \tSpecify the device: 'gpu' or 'cpu'\n"
-      << std::endl;
+  std::cerr << "Usage: <option(s)> VALUES"
+            << "Options:\n"
+            << "\t-h,--help\t\tShow this help message\n"
+            << "\t-t,--tracks \tSpecify the number of tracks\n"
+            << "\t-p,--pt \tSpecify the pt of particle\n"
+            << "\t-o,--output \tIndicator for writing propagation results\n"
+            << "\t-d,--device \tSpecify the device: 'gpu' or 'cpu'\n"
+            << std::endl;
 }
 
 // Struct for B field
@@ -55,8 +56,9 @@ int main(int argc, char *argv[]) {
   }
   std::vector<std::string> sources;
   unsigned int nTracks;
-  std::string outputPath = ".";
+  bool output = false;
   std::string device;
+  double pT;
   for (int i = 1; i < argc; ++i) {
     std::string arg = argv[i];
     if ((arg == "-h") or (arg == "--help")) {
@@ -65,8 +67,10 @@ int main(int argc, char *argv[]) {
     } else if (i + 1 < argc) {
       if ((arg == "-t") or (arg == "--tracks")) {
         nTracks = atoi(argv[++i]);
+      } else if ((arg == "-p") or (arg == "--pt")) {
+        pT = atof(argv[++i]);
       } else if ((arg == "-o") or (arg == "--output")) {
-        outputPath = argv[++i];
+        output = (atoi(argv[++i]) == 1);
       } else if ((arg == "-d") or (arg == "--device")) {
         device = argv[++i];
       } else {
@@ -77,7 +81,7 @@ int main(int argc, char *argv[]) {
   }
 
   std::cout << "----- Propgation test of " << nTracks << " tracks on " << device
-            << ". Results are writing to 'path' " << outputPath << " ----- "
+            << ". Writing results to obj file? " << output << " ----- "
             << std::endl;
 
   // Construct a stepper
@@ -89,13 +93,15 @@ int main(int argc, char *argv[]) {
   propOptions.maxSteps = 1000;
 
   // Construct random starting track parameters
-  std::normal_distribution<double> gauss(0., 1.);
   std::default_random_engine generator(42);
+  std::normal_distribution<double> gauss(0., 1.);
+  std::uniform_real_distribution<double> unif(-1.0 * M_PI, M_PI);
   TrackParameters pars[nTracks];
   for (int i = 0; i < nTracks; i++) {
     Vector3D rPos(0.1 * gauss(generator), 0.1 * gauss(generator),
                   0); // Units: mm
-    Vector3D rMom(0.5 * gauss(generator), 0.5 * gauss(generator),
+    double phi = unif(generator);
+    Vector3D rMom(pT * cos(phi), pT * sin(phi),
                   1); // Units: GeV
     double q = 1;
     TrackParameters rStart(rPos, rMom, q);
@@ -119,8 +125,8 @@ int main(int argc, char *argv[]) {
     TrackParameters *d_pars;
     PropResultType *d_ress;
 
-    cudaMalloc((void **)&d_propagator, sizeof(PropagatorType));
-    cudaMalloc((void **)&d_opt, sizeof(PropagatorOptions));
+    cudaMalloc(&d_propagator, sizeof(PropagatorType));
+    cudaMalloc(&d_opt, sizeof(PropagatorOptions));
     cudaMalloc(&d_pars, nTracks * sizeof(TrackParameters));
     cudaMalloc(&d_ress, nTracks * sizeof(PropResultType));
 
@@ -159,26 +165,29 @@ int main(int argc, char *argv[]) {
   std::cout << "Time (sec) to run propagation tests: "
             << elapsed_seconds.count() << std::endl;
 
-  // Write result to obj file
-  std::cout << "Writing yielded " << nTracks << " tracks to obj files..."
-            << std::endl;
+  if (output) {
+    // Write result to obj file
+    std::cout << "Writing yielded " << nTracks << " tracks to obj files..."
+              << std::endl;
 
-  for (int it = 0; it < nTracks; it++) {
-    PropResultType res = ress[it];
-    std::ofstream obj_track;
-    std::string fileName = outputPath + "/Track-" + std::to_string(it) + ".obj";
-    obj_track.open(fileName.c_str());
+    for (int it = 0; it < nTracks; it++) {
+      PropResultType res = ress[it];
+      std::ofstream obj_track;
+      std::string fileName =
+          device + "_output/Track-" + std::to_string(it) + ".obj";
+      obj_track.open(fileName.c_str());
 
-    for (int iv = 0; iv < res.steps(); iv++) {
-      obj_track << "v " << res.position.col(iv).x() << " "
-                << res.position.col(iv).y() << " " << res.position.col(iv).z()
-                << std::endl;
+      for (int iv = 0; iv < res.steps(); iv++) {
+        obj_track << "v " << res.position.col(iv).x() << " "
+                  << res.position.col(iv).y() << " " << res.position.col(iv).z()
+                  << std::endl;
+      }
+      for (unsigned int iv = 2; iv <= res.steps(); ++iv) {
+        obj_track << "l " << iv - 1 << " " << iv << std::endl;
+      }
+
+      obj_track.close();
     }
-    for (unsigned int iv = 2; iv <= res.steps(); ++iv) {
-      obj_track << "l " << iv - 1 << " " << iv << std::endl;
-    }
-
-    obj_track.close();
   }
 
   std::cout << "------------------------  ending  -----------------------"
