5cf472e815e208ec3db5bf30a3cf1b630a824197
xai <xiaocong.ai@cern.ch>
2020-11-16 15:10:58 +0100

    Port the LineSurface

---
+++ include/Surfaces/LineSurface.hpp
@@ -0,0 +1,264 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2016-2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+#include "Geometry/GeometryContext.hpp"
+#include "Geometry/GeometryStatics.hpp"
+#include "Surfaces/InfiniteBounds.hpp"
+#include "Surfaces/Surface.hpp"
+#include "Utilities/Definitions.hpp"
+
+namespace Acts {
+
+///  @class LineSurface without infinite bounds
+///
+///  Base class for a linear surfaces in the TrackingGeometry
+///  to describe dirft tube, straw like detectors or the Perigee
+///  It inherits from Surface.
+///
+///  @note It leaves the type() method virtual, so it can not be instantiated
+///
+/// @image html LineSurface.png
+class LineSurface : public Surface {
+  friend Surface;
+
+public:
+  using SurfaceBoundsType = InfiniteBounds;
+
+public:
+  //// Default Constructor
+  LineSurface() = default;
+
+  /// Copy constructor
+  ///
+  /// @param other The source surface for copying
+  ACTS_DEVICE_FUNC LineSurface(const LineSurface &other);
+
+  /// Copy constructor - with shift
+  ///
+  /// @param gctx The current geometry context object, e.g. alignment
+  /// @param other is the source cone surface
+  /// @param shift is the additional transform applied after copying
+  ACTS_DEVICE_FUNC LineSurface(const GeometryContext &gctx,
+                               const LineSurface &other,
+                               const Transform3D &shift);
+
+public:
+  ~LineSurface() = default;
+
+  /// Assignment operator
+  ///
+  /// @param slsf is the source surface dor copying
+  ACTS_DEVICE_FUNC LineSurface &operator=(const LineSurface &other);
+
+  /// Normal vector return
+  ///
+  /// @param gctx The current geometry context object, e.g. alignment
+  /// @param lposition is the local position is ignored
+  ///
+  /// @return a Vector3D by value
+  ACTS_DEVICE_FUNC Vector3D normal(const GeometryContext &gctx,
+                                   const Vector2D &lposition) const;
+
+  /// Normal vector return without argument
+  using Surface::normal;
+
+  /// The binning position is the position calcualted
+  /// for a certain binning type
+  ///
+  /// @param gctx The current geometry context object, e.g. alignment
+  /// @param bValue is the binning type to be used
+  ///
+  /// @return position that can beused for this binning
+  Vector3D binningPosition(const GeometryContext &gctx,
+                           BinningValue bValue) const final;
+
+  /// Return the surface type
+  ACTS_DEVICE_FUNC SurfaceType type() const;
+
+  /// This method returns the bounds of the Surface by reference */
+  ACTS_DEVICE_FUNC const InfiniteBounds *bounds() const;
+
+  /// Return the measurement frame - this is needed for alignment, in particular
+  ///
+  /// for StraightLine and Perigee Surface
+  ///  - the default implementation is the the RotationMatrix3D of the transform
+  ///
+  /// @param gctx The current geometry context object, e.g. alignment
+  /// @param position is the global position where the measurement frame is
+  /// constructed
+  /// @param momentum is the momentum used for the measurement frame
+  /// construction
+  ///
+  /// @return is a rotation matrix that indicates the measurement frame
+  ACTS_DEVICE_FUNC const RotationMatrix3D
+  referenceFrame(const GeometryContext &gctx, const Vector3D &position,
+                 const Vector3D &momentum) const;
+
+  /// Initialize the jacobian from local to global
+  /// the surface knows best, hence the calculation is done here.
+  /// The jacobian is assumed to be initialised, so only the
+  /// relevant entries are filled
+  ///
+  /// @param gctx The current geometry context object, e.g. alignment
+  /// @param jacobian is the jacobian to be initialized
+  /// @param position is the global position of the parameters
+  /// @param direction is the direction at of the parameters
+  ///
+  /// @param pars is the paranmeters vector
+  ACTS_DEVICE_FUNC void initJacobianToGlobal(const GeometryContext &gctx,
+                                             BoundToFreeMatrix &jacobian,
+                                             const Vector3D &position,
+                                             const Vector3D &direction,
+                                             const BoundVector &pars) const;
+
+  /// Initialize the jacobian from global to local
+  /// the surface knows best, hence the calculation is done here.
+  /// The jacobian is assumed to be initialised, so only the
+  /// relevant entries are filled
+  ///
+  /// @note This is the same with the PlaneSurface. But without using the
+  /// virtual function, we have duplicate the code here
+  /// @param jacobian is the jacobian to be initialized
+  /// @param position is the global position of the parameters
+  /// @param direction is the direction at of the parameters
+  /// @param gctx The current geometry context object, e.g. alignment
+  ///
+  /// @return the transposed reference frame (avoids recalculation)
+  ACTS_DEVICE_FUNC const RotationMatrix3D initJacobianToLocal(
+      const GeometryContext &gctx, FreeToBoundMatrix &jacobian,
+      const Vector3D &position, const Vector3D &direction) const;
+
+  /// Calculate the form factors for the derivatives
+  /// the calculation is identical for all surfaces where the
+  /// reference frame does not depend on the direction
+  ///
+  /// @param gctx The current geometry context object, e.g. alignment
+  /// @param position is the position of the paramters in global
+  /// @param direction is the direction of the track
+  /// @param rft is the transposed reference frame (avoids recalculation)
+  /// @param jacobian is the transport jacobian
+  ///
+  /// @return a five-dim vector
+  ACTS_DEVICE_FUNC const BoundRowVector
+  derivativeFactors(const GeometryContext &gctx, const Vector3D &position,
+                    const Vector3D &direction, const RotationMatrix3D &rft,
+                    const BoundToFreeMatrix &jacobian) const;
+
+  /// Local to global transformation
+  /// for line surfaces the momentum is used in order to interpret the drift
+  /// radius
+  ///
+  /// @param gctx The current geometry context object, e.g. alignment
+  /// @param lposition is the local position to be transformed
+  /// @param momentum is the global momentum (used to sign the closest approach)
+  /// @param position is the global position which is filled
+  ACTS_DEVICE_FUNC void localToGlobal(const GeometryContext &gctx,
+                                      const Vector2D &lposition,
+                                      const Vector3D &momentum,
+                                      Vector3D &position) const;
+
+  /// Specified for LineSurface: global to local method without dynamic
+  /// memory allocation
+  ///
+  /// This method is the true global->local transformation.<br>
+  /// makes use of globalToLocal and indicates the sign of the Acts::eBoundLoc0
+  /// by the given momentum
+  ///
+  /// The calculation of the sign of the radius (or \f$ d_0 \f$) can be done as
+  /// follows:<br>
+  /// May \f$ \vec d = \vec m - \vec c \f$ denote the difference between the
+  /// center of the line and the global position of the measurement/predicted
+  /// state,
+  /// then \f$ \vec d
+  /// \f$
+  /// lies within the so
+  /// called measurement plane.
+  /// The measurement plane is determined by the two orthogonal vectors \f$
+  /// \vec{measY}= \vec{Acts::eBoundLoc1} \f$
+  /// and \f$ \vec{measX} = \vec{measY} \times \frac{\vec{p}}{|\vec{p}|}
+  /// \f$.<br>
+  ///
+  /// The sign of the radius (\f$ d_{0} \f$ ) is then defined by the projection
+  /// of
+  /// \f$ \vec{d} \f$
+  /// onto \f$ \vec{measX} \f$:<br>
+  /// \f$ sign = -sign(\vec{d} \cdot \vec{measX}) \f$
+  ///
+  /// \image html SignOfDriftCircleD0.gif
+  ///
+  /// @param gctx The current geometry context object, e.g. alignment
+  /// @param position global 3D position - considered to be on surface but not
+  /// inside bounds (check is done)
+  /// @param momentum global 3D momentum representation (optionally ignored)
+  /// @param lposition local 2D position to be filled (given by reference for
+  /// method symmetry)
+  ///
+  /// @return boolean indication if operation was successful (fail means global
+  /// position was not on surface)
+  ACTS_DEVICE_FUNC bool globalToLocal(const GeometryContext &gctx,
+                                      const Vector3D &position,
+                                      const Vector3D &momentum,
+                                      Vector2D &lposition) const;
+
+  /// the pathCorrection for derived classes with thickness
+  /// is by definition 1 for LineSurfaces
+  ///
+  /// @note input parameters are ignored
+  /// @note there's no material associated to the line surface
+  ACTS_DEVICE_FUNC double pathCorrection(const GeometryContext &gctx,
+                                         const Vector3D &position,
+                                         const Vector3D &momentum) const;
+
+  /// @brief Straight line intersection schema
+  ///
+  /// @param gctx The current geometry context object, e.g. alignment
+  /// @param position The global position as a starting point
+  /// @param direction The global direction at the starting point
+  ///        @note exptected to be normalized
+  /// @param bcheck The boundary check directive for the estimate
+  ///
+  ///   <b>mathematical motivation:</b>
+  ///   Given two lines in parameteric form:<br>
+  ///   - @f$ \vec l_{a}(\lambda) = \vec m_a + \lambda \cdot \vec e_{a} @f$ <br>
+  ///   - @f$ \vec l_{b}(\mu) = \vec m_b + \mu \cdot \vec e_{b} @f$ <br>
+  ///   the vector between any two points on the two lines is given by:
+  ///   - @f$ \vec s(\lambda, \mu) = \vec l_{b} - l_{a} = \vec m_{ab} + \mu
+  ///   \cdot
+  ///  \vec e_{b} - \lambda \cdot \vec e_{a} @f$, <br>
+  ///   when @f$ \vec m_{ab} = \vec m_{b} - \vec m_{a} @f$.<br>
+  ///   @f$ \vec s(u, \mu_0) @f$  denotes the vector between the two
+  ///  closest points <br>
+  ///   @f$ \vec l_{a,0} = l_{a}(u) @f$ and @f$ \vec l_{b,0} =
+  ///  l_{b}(\mu_0) @f$ <br>
+  ///   and is perpendicular to both, @f$ \vec e_{a} @f$ and @f$ \vec e_{b} @f$.
+  ///
+  ///   This results in a system of two linear equations:<br>
+  ///   - (i) @f$ 0 = \vec s(u, \mu_0) \cdot \vec e_a = \vec m_ab \cdot
+  ///  \vec e_a + \mu_0 \vec e_a \cdot \vec e_b - u @f$ <br>
+  ///   - (ii) @f$ 0 = \vec s(u, \mu_0) \cdot \vec e_b = \vec m_ab \cdot
+  ///  \vec e_b + \mu_0  - u \vec e_b \cdot \vec e_a @f$ <br>
+  ///
+  ///   Solving (i), (ii) for @f$ u @f$ and @f$ \mu_0 @f$ yields:
+  ///   - @f$ u = \frac{(\vec m_ab \cdot \vec e_a)-(\vec m_ab \cdot \vec
+  ///  e_b)(\vec e_a \cdot \vec e_b)}{1-(\vec e_a \cdot \vec e_b)^2} @f$ <br>
+  ///   - @f$ \mu_0 = - \frac{(\vec m_ab \cdot \vec e_b)-(\vec m_ab \cdot \vec
+  ///  e_a)(\vec e_a \cdot \vec e_b)}{1-(\vec e_a \cdot \vec e_b)^2} @f$ <br>
+  ///
+  /// @return is the intersection object
+  ACTS_DEVICE_FUNC SurfaceIntersection intersect(
+      const GeometryContext &gctx, const Vector3D &position,
+      const Vector3D &direction, const BoundaryCheck &bcheck = false) const;
+
+  /// Return properly formatted class name for screen output */
+  // std::string name() const override;
+};
+
+#include "Acts/Surfaces/detail/LineSurface.ipp"
+
+} // namespace Acts
---
+++ include/Surfaces/detail/LineSurface.ipp
@@ -0,0 +1,242 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2018-2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+inline LineSurface::LineSurface(const Transform3D &transform)
+    : GeometryObject(), Surface(transform) {}
+
+inline LineSurface::LineSurface(const GeometryContext &gctx,
+                                const LineSurface &other,
+                                const Transform3D &shift)
+    : GeometryObject(), Surface(gctx, other, shift) {}
+
+inline LineSurface &LineSurface::operator=(const LineSurface &other) {
+  if (this != &other) {
+    Surface::operator=(other);
+  }
+  return *this;
+}
+
+inline Vector3D LineSurface::normal(const GeometryContext &gctx,
+                                    const Vector2D & /*lpos*/) const {
+  const auto &tMatrix = transform(gctx).matrix();
+  return Vector3D(tMatrix(0, 2), tMatrix(1, 2), tMatrix(2, 2));
+}
+
+inline Vector3D LineSurface::binningPosition(const GeometryContext &gctx,
+                                             BinningValue /*bValue*/) const {
+  return center(gctx);
+}
+
+inline Surface::SurfaceType LineSurface::type() const { return Surface::Line; }
+
+inline const InfiniteBounds *LineSurface::bounds() const { return nullptr; }
+
+inline RotationMatrix3D
+LineSurface::referenceFrame(const GeometryContext &gctx,
+                            const Vector3D & /*unused*/,
+                            const Vector3D &momentum) const {
+  RotationMatrix3D mFrame;
+  const auto &tMatrix = transform(gctx).matrix();
+  Vector3D measY(tMatrix(0, 2), tMatrix(1, 2), tMatrix(2, 2));
+  Vector3D measX(measY.cross(momentum).normalized());
+  Vector3D measDepth(measX.cross(measY));
+  // assign the columnes
+  mFrame.col(0) = measX;
+  mFrame.col(1) = measY;
+  mFrame.col(2) = measDepth;
+  // return the rotation matrix
+  return mFrame;
+}
+
+inline void LineSurface::initJacobianToGlobal(const GeometryContext &gctx,
+                                              BoundToFreeMatrix &jacobian,
+                                              const Vector3D &position,
+                                              const Vector3D &direction,
+                                              const BoundVector &pars) const {
+  // The trigonometry required to convert the direction to spherical
+  // coordinates and then compute the sines and cosines again can be
+  // surprisingly expensive from a performance point of view.
+  //
+  // Here, we can avoid it because the direction is by definition a unit
+  // vector, with the following coordinate conversions...
+  const double x = direction(0); // == cos(phi) * sin(theta)
+  const double y = direction(1); // == sin(phi) * sin(theta)
+  const double z = direction(2); // == cos(theta)
+
+  // ...which we can invert to directly get the sines and cosines:
+  const double cos_theta = z;
+  const double sin_theta = sqrt(x * x + y * y);
+  const double inv_sin_theta = 1. / sin_theta;
+  const double cos_phi = x * inv_sin_theta;
+  const double sin_phi = y * inv_sin_theta;
+  // retrieve the reference frame
+  const auto rframe = referenceFrame(gctx, position, direction);
+  // the local error components - given by the reference frame
+  jacobian.topLeftCorner<3, 2>() = rframe.topLeftCorner<3, 2>();
+  // the time component
+  jacobian(3, eT) = 1;
+  // the momentum components
+  jacobian(4, ePHI) = (-sin_theta) * sin_phi;
+  jacobian(4, eTHETA) = cos_theta * cos_phi;
+  jacobian(5, ePHI) = sin_theta * cos_phi;
+  jacobian(5, eTHETA) = cos_theta * sin_phi;
+  jacobian(6, eTHETA) = (-sin_theta);
+  jacobian(7, eQOP) = 1;
+
+  // the projection of direction onto ref frame normal
+  double ipdn = 1. / direction.dot(rframe.col(2));
+  // build the cross product of d(D)/d(ePHI) components with y axis
+  auto dDPhiY = rframe.block<3, 1>(0, 1).cross(jacobian.block<3, 1>(4, ePHI));
+  // and the same for the d(D)/d(eTheta) components
+  auto dDThetaY =
+      rframe.block<3, 1>(0, 1).cross(jacobian.block<3, 1>(4, eTHETA));
+  // and correct for the x axis components
+  dDPhiY -= rframe.block<3, 1>(0, 0) * (rframe.block<3, 1>(0, 0).dot(dDPhiY));
+  dDThetaY -=
+      rframe.block<3, 1>(0, 0) * (rframe.block<3, 1>(0, 0).dot(dDThetaY));
+  // set the jacobian components for global d/ phi/Theta
+  jacobian.block<3, 1>(0, ePHI) = dDPhiY * pars[eLOC_0] * ipdn;
+  jacobian.block<3, 1>(0, eTHETA) = dDThetaY * pars[eLOC_0] * ipdn;
+}
+
+inline const RotationMatrix3D LineSurface::initJacobianToLocal(
+    const GeometryContext &gctx, FreeToBoundMatrix &jacobian,
+    const Vector3D &position, const Vector3D &direction) const {
+  // Optimized trigonometry on the propagation direction
+  const double x = direction(0); // == cos(phi) * sin(theta)
+  const double y = direction(1); // == sin(phi) * sin(theta)
+  const double z = direction(2); // == cos(theta)
+  // can be turned into cosine/sine
+  const double cosTheta = z;
+  const double sinTheta = sqrt(x * x + y * y);
+  const double invSinTheta = 1. / sinTheta;
+  const double cosPhi = x * invSinTheta;
+  const double sinPhi = y * invSinTheta;
+  // The measurement frame of the surface
+  RotationMatrix3D rframeT =
+      referenceFrame(gctx, position, direction).transpose();
+  // given by the refernece frame
+  jacobian.block<2, 3>(0, 0) = rframeT.block<2, 3>(0, 0);
+  // Time component
+  jacobian(eT, 3) = 1;
+  // Directional and momentum elements for reference frame surface
+  jacobian(ePHI, 4) = -sinPhi * invSinTheta;
+  jacobian(ePHI, 5) = cosPhi * invSinTheta;
+  jacobian(eTHETA, 4) = cosPhi * cosTheta;
+  jacobian(eTHETA, 5) = sinPhi * cosTheta;
+  jacobian(eTHETA, 6) = -sinTheta;
+  jacobian(eQOP, 7) = 1;
+  // return the frame where this happened
+  return rframeT;
+}
+
+inline const BoundRowVector LineSurface::derivativeFactors(
+    const GeometryContext &gctx, const Vector3D &position,
+    const Vector3D &direction, const RotationMatrix3D &rft,
+    const BoundToFreeMatrix &jacobian) const {
+  // the vector between position and center
+  ActsRowVectorD<3> pc = (position - center(gctx)).transpose();
+  // the longitudinal component vector (alogn local z)
+  ActsRowVectorD<3> locz = rft.block<1, 3>(1, 0);
+  // build the norm vector comonent by subtracting the longitudinal one
+  double long_c = locz * direction;
+  ActsRowVectorD<3> norm_vec = direction.transpose() - long_c * locz;
+  // calculate the s factors for the dependency on X
+  const BoundRowVector s_vec =
+      norm_vec * jacobian.topLeftCorner<3, eBoundParametersSize>();
+  // calculate the d factors for the dependency on Tx
+  const BoundRowVector d_vec =
+      locz * jacobian.block<3, eBoundParametersSize>(4, 0);
+  // normalisation of normal & longitudinal components
+  double norm = 1. / (1. - long_c * long_c);
+  // create a matrix representation
+  ActsMatrixD<3, eBoundParametersSize> long_mat =
+      ActsMatrixD<3, eBoundParametersSize>::Zero();
+  long_mat.colwise() += locz.transpose();
+  // build the combined normal & longitudinal components
+  return (norm *
+          (s_vec - pc * (long_mat * d_vec.asDiagonal() -
+                         jacobian.block<3, eBoundParametersSize>(4, 0))));
+}
+
+inline Vector3D LineSurface::localToGlobal(const GeometryContext &gctx,
+                                           const Vector2D &lposition,
+                                           const Vector3D &momentum) const {
+  const auto &sTransform = transform(gctx);
+  const auto &tMatrix = sTransform.matrix();
+  Vector3D lineDirection(tMatrix(0, 2), tMatrix(1, 2), tMatrix(2, 2));
+
+  // get the vector perpendicular to the momentum and the straw axis
+  Vector3D radiusAxisGlobal(lineDirection.cross(momentum));
+  Vector3D locZinGlobal = sTransform * Vector3D(0., 0., lposition[eBoundLoc1]);
+  // add eBoundLoc0 * radiusAxis
+  return Vector3D(locZinGlobal +
+                  lposition[eBoundLoc0] * radiusAxisGlobal.normalized());
+}
+
+inline Result<Vector2D> LineSurface::globalToLocal(const GeometryContext &gctx,
+                                                   const Vector3D &position,
+                                                   const Vector3D &momentum,
+                                                   double /*tolerance*/) const {
+  using VectorHelpers::perp;
+  const auto &sTransform = transform(gctx);
+  const auto &tMatrix = sTransform.matrix();
+  Vector3D lineDirection(tMatrix(0, 2), tMatrix(1, 2), tMatrix(2, 2));
+  // Bring the global position into the local frame
+  Vector3D loc3Dframe = sTransform.inverse() * position;
+  // construct localPosition with sign*perp(candidate) and z.()
+  Vector2D lposition(perp(loc3Dframe), loc3Dframe.z());
+  Vector3D sCenter(tMatrix(0, 3), tMatrix(1, 3), tMatrix(2, 3));
+  Vector3D decVec(position - sCenter);
+  // assign the right sign
+  double sign = ((lineDirection.cross(momentum)).dot(decVec) < 0.) ? -1. : 1.;
+  lposition[eBoundLoc0] *= sign;
+  return Result<Vector2D>::success(lposition);
+}
+
+inline double LineSurface::pathCorrection(const GeometryContext & /*unused*/,
+                                          const Vector3D & /*pos*/,
+                                          const Vector3D & /*mom*/) const {
+  return 1.;
+}
+
+inline SurfaceIntersection
+LineSurface::intersect(const GeometryContext &gctx, const Vector3D &position,
+                       const Vector3D &direction,
+                       const BoundaryCheck & /*bcheck*/) const {
+  // following nominclature found in header file and doxygen documentation
+  // line one is the straight track
+  const Vector3D &ma = position;
+  const Vector3D &ea = direction;
+  // line two is the line surface
+  const auto &tMatrix = transform(gctx).matrix();
+  Vector3D mb = tMatrix.block<3, 1>(0, 3).transpose();
+  Vector3D eb = tMatrix.block<3, 1>(0, 2).transpose();
+  // now go ahead and solve for the closest approach
+  Vector3D mab(mb - ma);
+  double eaTeb = ea.dot(eb);
+  double denom = 1 - eaTeb * eaTeb;
+  // validity parameter
+  Intersection3D::Status status = Intersection3D::Status::unreachable;
+  if (denom * denom > s_onSurfaceTolerance * s_onSurfaceTolerance) {
+    double u = (mab.dot(ea) - mab.dot(eb) * eaTeb) / denom;
+    // Check if we are on the surface already
+    status = (u * u < s_onSurfaceTolerance * s_onSurfaceTolerance)
+                 ? Intersection3D::Status::onSurface
+                 : Intersection3D::Status::reachable;
+    Vector3D result = (ma + u * ea);
+    // Evaluate the boundary check if requested
+    // m_bounds == nullptr prevents unecessary calulations for PerigeeSurface
+    return {Intersection3D(result, u, status), this};
+  }
+  // return a false intersection
+  return {Intersection3D(position, std::numeric_limits<double>::max(), status),
+          this};
+}
+
+// inline std::string LineSurface::name() const { return "Acts::LineSurface"; }
