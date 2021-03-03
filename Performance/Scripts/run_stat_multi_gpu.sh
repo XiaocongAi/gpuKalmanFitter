122147d448d21c3ad9c9a71ec110dcbf507820c9
Georgiana Mania <georgiana.mania@gmail.com>
2021-03-03 09:28:28 +0100

    add stats for multi-gpu

--- Performance/Scripts/run_stat_multi_gpu.sh
+++ Performance/Scripts/run_stat_multi_gpu.sh
@@ -9,7 +9,7 @@ fi
 #The input parameter is the location of the executable
 echo "Start statistical runs for $1"
 
-nStreams=(1) 
+nStreams=(2) 
 
 nTracks=(5 10 50 100 500 1000 5000 10000 50000 100000) 
 gridSizes=( '100000')
@@ -31,12 +31,14 @@ for i in ${nTracks[@]}; do
 		for cnt in {1..5}; do
 			echo "Run $cnt for ${i} tracks with gridSize=${gridSizes[j]} and blockSize=${blockSizes[j]} and ${k} streams:"
                         
-			echo " multi gpu  $1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s 1 -u"
+		#	echo " multi gpu  $1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s 1 -u 1"
 			$1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s 1 -u 1
 	                
                       #  echo " single gpu  $1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -s 1" 
 			$1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s 1 
-                
+				
+	                $1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s 1 -u 0
+        
 			sleep 1;
 		done
 	    done
