d0c77e1b9f170151d1a6c10d59c41e9fe759b9c0
Georgiana Mania <georgiana.mania@gmail.com>
2021-04-14 11:06:15 +0200

    update multi-gpu script

--- Performance/Scripts/run_stat_multi_gpu.sh
+++ Performance/Scripts/run_stat_multi_gpu.sh
@@ -9,25 +9,24 @@ fi
 #The input parameter is the location of the executable
 echo "Start statistical runs for $1"
 
-nStreams=(2) 
+nStreams=(1) 
 
 nTracks=(5 10 50 100 500 1000 5000 10000 50000 100000) 
-gridSizes=( '100000')
+gridSizes=('5120')  #'100000')
 blockSizes=( '8x8')
+sharedMemory=0
 
 for i in ${nTracks[@]}; do
 	for ((j=0; j<${#gridSizes[@]};++j)); do
             for k in ${nStreams[@]}; do
-		for cnt in {1..5}; do
+		for cnt in {1..10}; do
 			echo "Run $cnt for ${i} tracks with gridSize=${gridSizes[j]} and blockSize=${blockSizes[j]} and ${k} streams:"
                         
-		#	echo " multi gpu  $1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s 1 -u 1"
-			$1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s 1 -u 1
+			echo " multi gpu  $1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s ${sharedMemory} -u 1"
+			$1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s ${sharedMemory} -u 1
 	                
-                      #  echo " single gpu  $1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -s 1" 
-			$1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s 1 
-				
-	                $1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s 1 -u 0
+                        echo " single gpu  $1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s ${sharedMemory} -u 0" 
+			$1 -t ${i} -e ${k} -d "gpu" -o 0 -g ${gridSizes[j]} -b ${blockSizes[j]} -s ${sharedMemory} -u 0
         
 			sleep 1;
 		done
